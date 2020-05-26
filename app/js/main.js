83dc86e5923e60fc42a69af87993934fa4c65637
Kerthin <belichenko.roden@list.ru>
2020-05-26 15:37:30 +0300

    Creating a function for entering mathematical operators

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,44 @@ document.querySelectorAll("._number").forEach(function (elem) {
     var n = elem.innerText;
     elem.onclick = function (event) { return result.value = result.value + n; };
 });
+var buffer = [];
+var evaluate = function (buffer) {
+    var secondArgument = buffer.pop().value;
+    var operator = buffer.pop().value;
+    var firstArgument = buffer.pop().value;
+    if (operator === "plus") {
+        return firstArgument + secondArgument;
+    }
+    else if (operator === "minus") {
+        return firstArgument - secondArgument;
+    }
+    else if (operator === "multiplication") {
+        return firstArgument * secondArgument;
+    }
+    else if (operator === "divide") {
+        return firstArgument / secondArgument;
+    }
+    else if (operator === "percent") {
+        return firstArgument % secondArgument;
+    }
+};
+var opCallback = function (actionName) {
+    return function (event) {
+        if (buffer && buffer.length) {
+            buffer.push({ type: "value", value: parseFloat(result_1.value, 10) });
+            var result_1 = evaluate(buffer);
+            buffer.push({ type: "value", value: result_1 });
+            buffer.push({ type: "action", value: actionName });
+            result_1.value = "";
+        }
+        else {
+            buffer.push({ type: "value", value: parseFloat(result.value, 10) });
+            buffer.push({ type: "action", value: actionName });
+            result.value = "";
+        }
+    };
+};
+for (var _i = 0, _a = ["plus", "minus", "multiplication", "divide", "percent"]; _i < _a.length; _i++) {
+    var actionName = _a[_i];
+    document.querySelector("._action[action=" + actionName + "]").onclick = opCallback(actionName);
+}
--- app/js/main.ts
+++ app/js/main.ts
@@ -6,3 +6,50 @@ document.querySelectorAll("._number").forEach(
     elem.onclick = event => result.value = result.value + n
   }
 )
+
+const buffer = []
+
+const evaluate = buffer => {
+  const secondArgument = buffer.pop().value
+  const operator = buffer.pop().value
+  const firstArgument = buffer.pop().value
+  
+  if (operator === "plus") {
+    return firstArgument + secondArgument
+  }
+  else if (operator === "minus") {
+    return firstArgument - secondArgument
+  }
+  else if (operator === "multiplication") {
+    return firstArgument * secondArgument
+  }
+  else if (operator === "divide") {
+    return firstArgument / secondArgument
+  }
+  else if (operator === "percent") {
+    return firstArgument % secondArgument
+  }
+}
+
+const opCallback = actionName =>
+  event => {
+    if (buffer && buffer.length) {
+      buffer.push({ type: "value", value: parseFloat(result.value, 10) })
+      
+      const result = evaluate(buffer)
+      
+      buffer.push({ type: "value", value: result })
+      buffer.push({ type: "action", value: actionName })
+      
+      result.value = ""
+    }
+    else {
+      buffer.push({ type: "value", value: parseFloat(result.value, 10) })
+      buffer.push({ type: "action", value: actionName })
+      result.value = ""
+    }
+  }
+
+for (const actionName of [ "plus", "minus", "multiplication", "divide", "percent" ]) {
+  document.querySelector(`._action[action=${actionName}]`).onclick = opCallback(actionName)
+}
