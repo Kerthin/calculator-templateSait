5bec1b19c55ab3e15c38dc1676ad3e4e3a6bb1d0
Kerthin <belichenko.roden@list.ru>
2021-07-01 13:12:22 +0300

    Create file components for HRML and CSS tamplates. Also rename folder "dist" to "docs" for "Github Pages"

--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,142 @@
 // sudo npm install --save-dev gulp-sourcemaps gulp-imagemin gulp-autoprefixer imagemin-pngquant gulp-uglify gulp-rigger gulp-minify-css rimraf gulp-watch gulp-pug gulp-sass browser-sync
-var 	gulp 			= 	require('gulp')					,
-		sourcemaps 		= 	require('gulp-sourcemaps')		,
-		imagemin 		= 	require('gulp-imagemin')		,
-		prefixer 		= 	require('gulp-autoprefixer')	,
-		pngquant 		= 	require('imagemin-pngquant')	,
-		uglify 			= 	require('gulp-uglify')			,
-		rigger 			= 	require('gulp-rigger')			,
-		cssmin 			= 	require('gulp-minify-css')		,
-		rimraf 			= 	require('rimraf')				,
-		watch 			= 	require('gulp-watch')			,
-		pug				=	require('gulp-pug')				,
-		sass 			= 	require('gulp-sass')			,
-		browserSync 	= 	require('browser-sync')			,
-		reload 			= 	browserSync.reload				;
+var 	gulp = require('gulp'),
+		sourcemaps = require('gulp-sourcemaps'),
+		imagemin = require('gulp-imagemin'),
+		prefixer = require('gulp-autoprefixer'),
+		pngquant = require('imagemin-pngquant'),
+		uglify = require('gulp-uglify'),
+		rigger = require('gulp-rigger'),
+		cssmin = require('gulp-minify-css'),
+		rimraf = require('rimraf'),
+		watch = require('gulp-watch'),
+		pug = require('gulp-pug'),
+		sass = require('gulp-sass'),
+		browserSync = require('browser-sync'),
+		reload = browserSync.reload;
 
 var		path 			= {
-			dist: 			{
-				image: 	'dist/image/'	,
-				fonts: 	'dist/fonts/'	,
-				html: 	'dist/'			,
-				css: 	'dist/css/'		,
-				js: 	'dist/js/'
+			docs: 			{
+				image: 'docs/image/',
+				fonts: 'docs/fonts/',
+				html: 'docs/',
+				css: 'docs/css',
+				js: 'docs/js/'
 			},
 			app:			{
-				image: 	'app/image/**/*.*'	,
-				fonts: 	'app/fonts/**/*.*'	,
-				scss: 	'app/scss/*.scss',
-				pug:	'app/*.pug'			,
-				js: 	'app/js/*.js'	
+				image: 'app/image/**/**/*.*',
+				fonts: 'app/fonts/**/*.*',
+				scss: 'app/scss/*.scss',
+				pug: 'app/*.pug',
+				js: 'app/js/*.js'	
 			},
 			watch: 			{
-				image: 	'app/image/**/*.*',
-				fonts: 	'app/fonts/**/*.*',
-				html: 	'app/**/*.html',
-				scss: 	'app/scss/*.scss',
-				pug:	'app/*.pug'			,
-				css: 	'app/css/*.css',
-				js: 	'app/js/*.js'
+				image: 'app/image/**/**/*.*',
+				fonts: 'app/fonts/**/*.*',
+				html: 'app/**/*.html',
+				scss: 'app/scss/*.scss',
+				pug: 'app/*.pug',
+				css: 'app/css/*.css',
+				js: 'app/js/*.js'
 			},
 			clean: 			{
-				dist:	'./dist'
+				docs: './docs'
 			}
 };
 
 var 	config			= {
 		server: {
-			baseDir:	"./dist"
+			baseDir: "./docs"
 		},
-		tunnel: 	 	true,
-		host: 			'localhost',
-		port: 		 	9000,
-		logPrefix: 		"Frontend_Devil"
+		tunnel: true,
+		host: 'localhost',
+		port: 9000,
+		logPrefix: "Frontend_Devil"
 };
 
-gulp.task('html:dist',	function()	{
+gulp.task('html:docs',	function()	{
 	gulp.src(path.app.pug)
 		.pipe(pug({
 			pretty: false
 		}))
-		.pipe(gulp.dest(path.dist.html))
-		.pipe(reload({stream:	true}));	//Перезагрузка сервера для обновлений
+		.pipe(gulp.dest(path.docs.html))
+		.pipe(reload({stream: true}));	//Перезагрузка сервера для обновлений
 });
 
-gulp.task('js:dist',	function()	{
+gulp.task('js:docs',	function()	{
 	gulp.src(path.app.js)
 		.pipe(rigger())
 		.pipe(sourcemaps.init())
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.dist.js))
+		.pipe(gulp.dest(path.docs.js))
 		.pipe(reload({stream:	true}));
 });
 
-gulp.task('style:dist',	function()	{
+gulp.task('style:docs',	function()	{
 	gulp.src(path.app.scss)
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(prefixer())
 		.pipe(cssmin())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.dist.css))
-		.pipe(reload({stream:	true}));
+		.pipe(gulp.dest(path.docs.css))
+		.pipe(reload({stream: true}));
 });
 
-gulp.task('image:dist',	function()	{
+gulp.task('image:docs',	function()	{
 	gulp.src(path.app.image)
 		.pipe(imagemin({
-			progressive:	true,
-			svgoPlugins:	[{removeViewBox:	false}],
-			use:			[pngquant()],
-			interlaced:		true 
+			progressive: true,
+			svgoPlugins: [{removeViewBox:	false}],
+			use: [pngquant()],
+			interlaced: true 
 		}))
-		.pipe(gulp.dest(path.dist.image))
-		.pipe(reload({stream:	true}));
+		.pipe(gulp.dest(path.docs.image))
+		.pipe(reload({stream: true}));
 });
 
-gulp.task('fonts:dist',	function()	{
+gulp.task('fonts:docs',	function()	{
 	gulp.src(path.app.fonts)
-		.pipe(gulp.dest(path.dist.fonts))
+		.pipe(gulp.dest(path.docs.fonts))
 });
 
-gulp.task('dist',	[
-	'html:dist'		,
-	'style:dist'	,
-	'js:dist'		,
-	'image:dist'	,
-	'fonts:dist'
+gulp.task('docs',	[
+	'html:docs',
+	'style:docs',
+	'js:docs',
+	'image:docs',
+	'fonts:docs'
 ]);
 
 gulp.task('watch',	function()	{
 	watch([path.watch.pug],	function(event,	cb)	{
-		gulp.start('html:dist');
+		gulp.start('html:docs');
 	});
 	watch([path.watch.scss],	function(event,	cb)	{
-		gulp.start('style:dist');
+		gulp.start('style:docs');
 	});
 	watch([path.watch.js],		function(event,	cb)	{
-		gulp.start('js:dist');
+		gulp.start('js:docs');
 	});
-	// watch([path.watch.image],	function(event,	cb)	{
-	// 	gulp.start('image:dist');
-	// });
+	watch([path.watch.image],	function(event,	cb)	{
+		gulp.start('image:docs');
+	});
 	watch([path.watch.fonts],	function(event,	cb)	{
-		gulp.start('fonts:dist');
+		gulp.start('fonts:docs');
 	});
 });
 
 gulp.task('webserver',	function()	{
 	browserSync({
-		server: { baseDir: "./dist" },
-		tunnel: false,
-		host: 'localhost',
-		port: 3000,
-		open: true,
-		logLevel: "silent",
-		notify: false,
-		logLevel: "info"
-	});
+     server: {
+        baseDir: "docs/",
+        directory: true
+     }
+   });
 });
 
 gulp.task('clean',		function(cb)	{
-	rimraf(path.clean.dist	,	cb);
+	rimraf(path.clean.docs, cb);
 });
 
-gulp.task('default',	['dist',	'webserver',	'watch']);
+gulp.task('default',	['docs',	'webserver',	'watch']);
